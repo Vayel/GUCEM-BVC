2dec89c02b7a249f9747db3f97674542278fb450
Jeremy <j.boucheteil@gmail.com>
2016-09-26 14:05:14 +0000

    typo

--- bvc/models/money_stock.py
+++ bvc/models/money_stock.py
@@ -3,4 +3,5 @@ from django.db import models
 class MoneyStock(models.Model):
     treasury = models.PositiveIntegerField(default=0)
     sold_cash = models.PositiveIntegerField(default=0)
-    sold_check = = models.PositiveIntegerField(default=0)
+    sold_check = models.PositiveIntegerField(default=0)
+    
