dba7b43a92512b9788d4f7b5a4427d0d1c001e6b
Vincent Lefoulon <vincent.lefoulon@free.fr>
2016-09-26 21:48:29 +0200

    Customize admin site

--- bvc/admin.py
+++ bvc/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
-from .models import command
+from . import models
 
-admin.site.register(command.MemberCommand)
-admin.site.register(command.CommissionCommand)
+class CommandAdmin(admin.ModelAdmin):
+    readonly_fields = ['datetime_placed']
+    exclude = []
+
+class MemberCommandAdmin(CommandAdmin):
+    list_display = ['lastname', 'firstname', 'grouped_command',
+                    'datetime_placed', 'state',]
+    list_filter = ['state',]
+
+class CommissionCommandAdmin(CommandAdmin):
+    list_display = ['commission', 'grouped_command', 'datetime_placed',
+                    'state',]
+    list_filter = ['commission', 'state',]
+
+class GroupedCommandAdmin(CommandAdmin):
+    list_display = ['datetime_placed', 'state', 'state',]
+    list_filter = ['state',]
+
+admin.site.register(models.command.MemberCommand, MemberCommandAdmin)
+admin.site.register(models.command.CommissionCommand, CommissionCommandAdmin)
+admin.site.register(models.command.GroupedCommand, GroupedCommandAdmin)
