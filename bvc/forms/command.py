6402e5a2cb0c69dfd5662778afccb978df773d2a
Vincent Lefoulon <vincent.lefoulon@free.fr>
2016-10-05 10:32:05 +0200

    Allow to label a command received

--- bvc/forms/command.py
+++ bvc/forms/command.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
 
 from ..models import command
 
@@ -16,3 +16,22 @@ class PlaceGroupedCommand(ModelForm):
     class Meta:
         model = command.GroupedCommand
         fields = ['placed_amount']
+        
+class ReceiveGroupedCommand(ModelForm):
+    class Meta:
+        model = command.GroupedCommand
+        fields = ['received_amount']
+
+    def clean_received_amount(self):
+        received_amount = self.cleaned_data['received_amount']
+
+        if received_amount > self.instance.placed_amount:
+            raise ValidationError('Le montant reçu ne peut être supérieur à ' +
+                                  'celui commandé')
+
+        return received_amount
+
+class PrepareGroupedCommand(ModelForm):
+    class Meta:
+        model = command.GroupedCommand
+        fields = ['prepared_amount']
--- bvc/views/manager.py
+++ bvc/views/manager.py
@@ -1,11 +1,61 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.conf import settings
 from django.template.loader import render_to_string
+from django.views.decorators.http import require_http_methods
+from django.utils.timezone import now
 
 from .. import forms
 from .. import utils
+from .. import models
+
+def list_grouped_commands(request):
+    # TODO: use model formset
+    
+    context = {
+        'placed_commands': models.command.GroupedCommand.objects.filter(
+            state=models.command.PLACED_STATE,
+        ),
+        'received_commands': models.command.GroupedCommand.objects.filter(
+            state=models.command.GroupedCommand.RECEIVED_STATE,
+        ),
+        'prepared_commands': models.command.GroupedCommand.objects.filter(
+            state=models.command.PREPARED_STATE,
+        ),
+        'receive_form': forms.command.ReceiveGroupedCommand(),
+        'prepare_form': forms.command.PrepareGroupedCommand(),
+    }
+
+    return render(request, 'bvc/list_grouped_commands.html', context)
+
+@require_http_methods(["POST"])
+def receive_grouped_command(request, pk):
+    command = get_object_or_404(models.command.GroupedCommand, pk=pk)
+    form = forms.command.ReceiveGroupedCommand(request.POST, instance=command)
+
+    if form.is_valid():
+        form.save()
+        command.datetime_received = now()
+        command.state = models.command.GroupedCommand.RECEIVED_STATE
+        command.save()
+
+        send_mail(
+            utils.format_mail_subject('Commande groupée reçue'),
+            render_to_string(
+                'bvc/mails/receive_grouped_command.txt',
+                {'pk': pk}
+            ),
+            settings.TREASURER_MAIL,
+            [settings.BVC_MANAGER_MAIL],
+            fail_silently=False,
+        )
+
+    return redirect('bvc:list_grouped_commands')
+
+@require_http_methods(["POST"])
+def prepare_grouped_command(request, pk):
+    return redirect('bvc:list_grouped_commands')
 
 def place_grouped_command(request):
     context = {}
@@ -29,7 +79,7 @@ def place_grouped_command(request):
 
             return redirect('bvc:place_grouped_command')
     else:
-        form = forms.command.PlaceGroupedCommand() 
+        form = forms.command.PlaceGroupedCommand(request.POST) 
 
     context['form'] = form
 
