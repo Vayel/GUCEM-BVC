ccbbed61116a77e876c74526905add0ed00c7aba
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-01-28 12:14:09 +0100

    Add auth models to admin site

--- bvc/admin/user.py
+++ bvc/admin/user.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.models import User, Group
 from django.contrib import admin
 
 from .site import admin_site
 from .. import models
 
 
+admin_site.register(User)
+admin_site.register(Group)
 admin_site.register(models.user.Member)
 admin_site.register(models.user.Commission)
