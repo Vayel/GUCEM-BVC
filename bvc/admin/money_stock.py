3fadb0825ad4023f9bd48e1fb71452832814de08
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-02-06 14:34:03 +0100

    Improve UI of treasury operations

--- bvc/admin/money_stock.py
+++ bvc/admin/money_stock.py
@@ -89,7 +89,9 @@ class CashBankDepositAdmin(admin.ModelAdmin):
 
 
 class TreasuryOperationAdmin(admin.ModelAdmin):
-    list_display = ['delta', 'reason',]
+    list_display = ['id', 'stock', 'delta', 'reason',]
+    list_display_links = None
+    list_editable = ['reason',]
     form = forms.money_stock.TreasuryOperationAdminForm
     fields = forms.money_stock.TreasuryOperationAdminForm.Meta.fields
 
@@ -101,6 +103,18 @@ class TreasuryOperationAdmin(admin.ModelAdmin):
         actions = super().get_actions(request)
         del actions['delete_selected']
         return actions
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['treasury'] = models.money_stock.get_treasury()
+
+        return super().changelist_view(request, extra_context=extra_context)
+
+    def add_view(self, request, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['treasury'] = models.money_stock.get_treasury()
+
+        return super().add_view(request, form_url=form_url, extra_context=extra_context)
 
 
 admin_site.register(models.BankDeposit, BankDepositAdmin)
