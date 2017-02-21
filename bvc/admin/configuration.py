53dd40cc1b8b16d1142c18f9a03efe8d58aa762a
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-02-21 16:49:36 +0100

    Remove delete action of configurations

--- bvc/admin/configuration.py
+++ bvc/admin/configuration.py
@@ -15,6 +15,12 @@ class ConfigurationAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False
+    
+    def get_actions(self, request):
+        # Remove delete action
+        actions = super().get_actions(request)
+        del actions['delete_selected']
+        return actions
 
 
 admin_site.register(models.configuration.Configuration, ConfigurationAdmin)
